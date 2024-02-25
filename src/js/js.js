24dd3a694ed99ed1ca8bed652c6ec85924f9a875
unknown <raficahyadi1221@gmail.com>
2024-02-25 19:12:18 +0700

    feat:homepage

--- src/js/js.js
+++ src/js/js.js
@@ -101,3 +101,15 @@ function searchTable() {
     }
   }
 }
+
+// user js
+window.onscroll = function () {
+  const header = document.querySelector("header")
+  const fixedNav = header.offsetTop
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("nav-fixed")
+  } else {
+    header.classList.remove("nav-fixed")
+  }
+};
